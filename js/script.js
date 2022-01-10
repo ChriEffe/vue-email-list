91d369da6919c3a2aba8be83994eb09aa5f1027f
ChriEffe <christianferrazzano93@gmail.com>
2022-01-10 15:24:08 +0100

    email random stampate

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all’interno di una lista.*/
 
+const app = new Vue({
+    el: "#app",
+    data: {
+        emails: [],
+    },
+    methods: {
+        rndEmail: function () {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        this.emails.push(response.data.response);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        }
+    },
+    created() {
+        this.rndEmail()
+    }
+})
